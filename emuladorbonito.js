4e580967b7258253335067e41778a712b2fe55be
Adrián Álvarez <adrianalvarezleon@gmail.com>
2019-11-28 17:11:54 +0000

    Half Adder implemented

--- emuladorbonito.js
+++ emuladorbonito.js
@@ -10,11 +10,75 @@
 //
 // }
 
+class HalfAdder{
+  constructor(){
+    this.carry = 0
+    this.sum = 0
+  }
+
+  or(a,b){
+    return Number(a) || Number(b)
+  }
+
+  and(a,b){
+    return Number(a) && Number(b)
+  }
+
+  xor(a,b){
+    return this.and(this.nand(a,b),
+                    this.or(a,b))
+  }
+
+  nand(a,b){
+    return this.not(this.and(a,b))
+  }
+
+  not(a){
+    if(Number(a) == 0){
+      return 1
+    }else return 0
+  }
+
+  /*
+
+   Como nuestra arquitectura no permite numeros negativos pero sin embargo
+   queremos realizar decrementos, he implementado esta puerta muy sencilla
+   para que se pueda restar sin tener que pasar a ningún complemento usando
+   los half adder.
+
+   Su tabla de verdad sería:
+   a | b | carry | sum
+   0 | 0 |   0   |  0
+   0 | 1 |   1   |  1
+   1 | 0 |   0   |  1
+   1 | 1 |   0   |  0
+
+   */
+
+  xpecialDoor(a,b){
+    return this.and(this.not(a),b)
+  }
+
+  add(a,b){
+      this.sum = this.xor(a,b)
+      this.carry = this.and(a,b)
+  }
+
+  sub(a,b){
+      this.sum = this.xor(a,b)
+      this.carry = this.xpecialDoor(a,b)
+  }
+}
+
+class FullAdder{
+  
+}
+
 WORD_SIZE = 32
 
 function decimalToBinary(decimalNumber){
   incompleteBinaryNumber = parseInt(decimalNumber, 10).toString(2)
-  completeBinaryNumber = append_zeroes(incompleteBinaryNumber)
+  completeBinaryNumber = appendZeroes(incompleteBinaryNumber)
   return completeBinaryNumber
 }
 
@@ -30,5 +94,6 @@ function appendZeroes(incompleteBinaryNumber){
 
 module.exports = {
   decimalToBinary,
-  appendZeroes
+  appendZeroes,
+  HalfAdder
 }
--- test_emuladorbonito.js
+++ test_emuladorbonito.js
@@ -6,10 +6,6 @@ const emuladorBonito = require('./emuladorbonito.js')
 
 describe('Binary Numbers', function(){
 
-  it("Negative numbers should throw an Error", function(){
-    assert.throws(function() {emuladorBonito.decimalToBinary(-1)}, Error);
-  })
-
   it("Positive decimal numbers should be converted to its binary counterpart.", function(){
     assert.equal(emuladorBonito.decimalToBinary(0),'00000000000000000000000000000000')
     assert.equal(emuladorBonito.decimalToBinary(1),'00000000000000000000000000000001')
@@ -22,4 +18,94 @@ describe('Binary Numbers', function(){
     assert.equal(emuladorBonito.decimalToBinary(662510872),'00100111011111010001110100011000')
     assert.equal(emuladorBonito.decimalToBinary(4294967295),'11111111111111111111111111111111')
   })
+});
+
+
+describe('Half Adder', function(){
+
+  before(function(){
+    halfAdder = new emuladorBonito.HalfAdder();
+  })
+
+  it("Primitive or function", function(){
+    assert.equal(halfAdder.or('0','0'), 0)
+    assert.equal(halfAdder.or('0','1'), 1)
+    assert.equal(halfAdder.or('1','0'), 1)
+    assert.equal(halfAdder.or('1','1'), 1)
+  });
+
+  it("Primitive and function", function(){
+    assert.equal(halfAdder.and('0','0'), 0)
+    assert.equal(halfAdder.and('0','1'), 0)
+    assert.equal(halfAdder.and('1','0'), 0)
+    assert.equal(halfAdder.and('1','1'), 1)
+  });
+
+  it("Primitive not function", function(){
+    assert.equal(halfAdder.not('1'), 0)
+    assert.equal(halfAdder.not('0'), 1)
+  });
+
+  it("Primitive nand function", function(){
+    assert.equal(halfAdder.nand('0','0'), 1)
+    assert.equal(halfAdder.nand('0','1'), 1)
+    assert.equal(halfAdder.nand('1','0'), 1)
+    assert.equal(halfAdder.nand('1','1'), 0)
+  });
+
+  it("Primitive xor function", function(){
+    assert.equal(halfAdder.xor('0','0'), 0)
+    assert.equal(halfAdder.xor('0','1'), 1)
+    assert.equal(halfAdder.xor('1','0'), 1)
+    assert.equal(halfAdder.xor('1','1'), 0)
+  });
+
+  it("Primitive xpecialDoor function", function(){
+    assert.equal(halfAdder.xpecialDoor('0','0'), 0)
+    assert.equal(halfAdder.xpecialDoor('0','1'), 1)
+    assert.equal(halfAdder.xpecialDoor('1','0'), 0)
+    assert.equal(halfAdder.xpecialDoor('1','1'), 0)
+  });
+
+  it("Add function", function(){
+
+    halfAdder.add(0,0)
+    assert.equal(halfAdder.sum, 0)
+    assert.equal(halfAdder.carry, 0)
+
+    halfAdder.add(1,0)
+    assert.equal(halfAdder.sum, 1)
+    assert.equal(halfAdder.carry, 0)
+
+    halfAdder.add(0,1)
+    assert.equal(halfAdder.sum, 1)
+    assert.equal(halfAdder.carry, 0)
+
+    halfAdder.add(1,1)
+    assert.equal(halfAdder.sum, 0)
+    assert.equal(halfAdder.carry, 1)
+
+  });
+
+  it("Sub function", function(){
+
+    halfAdder.sub(0,0)
+    assert.equal(halfAdder.sum, 0)
+    assert.equal(halfAdder.carry, 0)
+
+    halfAdder.sub(1,0)
+    assert.equal(halfAdder.sum, 1)
+    assert.equal(halfAdder.carry, 0)
+
+    halfAdder.sub(0,1)
+    assert.equal(halfAdder.sum, 1)
+    assert.equal(halfAdder.carry, 1)
+
+    halfAdder.sub(1,1)
+    assert.equal(halfAdder.sum, 0)
+    assert.equal(halfAdder.carry, 0)
+
+  });
+
+
 });
